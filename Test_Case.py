bceea82273e0d2ee23441b6e23db2e534f533c81
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-10-25 22:24:14 +0530

    Update Test_Case.py

--- Test_Case.py
+++ Test_Case.py
@@ -7,8 +7,6 @@ class TestingSum(unittest.TestCase):
     def test_sum_tuple(self):  
         self.assertEqual(sum((1, 1, 8)), 10, "It should be 10")
     def test_sum_tuple(self):  
-        self.assertEqual(sum((1, 1, 8)), 10, "It should be 10")
-    def test_sum_tuple(self):  
         self.assertEqual(sum((2, 2, 6)), 10, "It should be 10")
     # def test_sum_tuple(self):  
     #    self.assertEqual(sum((1, 2, 5)), 10, "It should be 10")  
