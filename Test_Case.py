4d4b977ebf925c8c238c5d24ec73385b6b8c33e9
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-10-25 17:08:40 +0530

    Update Test_Case.py

--- Test_Case.py
+++ Test_Case.py
@@ -4,12 +4,12 @@ class TestingSum(unittest.TestCase):
   
     def test_sum(self):  
         self.assertEqual(sum([2, 3, 5]), 10, "It should be 10")  
-    def test_sum_tuple(self):  
-       self.assertEqual(sum((1, 3, 5)), 10, "It should be 10")
-    def test_sum_tuple(self):  
-       self.assertEqual(sum((1, 1, 5)), 10, "It should be 10")
-    def test_sum_tuple(self):  
-       self.assertEqual(sum((1, 2, 5)), 10, "It should be 10")  
+    # def test_sum_tuple(self):  
+    #    self.assertEqual(sum((1, 3, 5)), 10, "It should be 10")
+    # def test_sum_tuple(self):  
+    #    self.assertEqual(sum((1, 1, 5)), 10, "It should be 10")
+    # def test_sum_tuple(self):  
+    #    self.assertEqual(sum((1, 2, 5)), 10, "It should be 10")  
   
 if __name__ == '__main__':  
     unittest.main() 
